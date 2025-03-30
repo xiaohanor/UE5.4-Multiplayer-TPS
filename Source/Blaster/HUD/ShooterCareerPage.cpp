fc278c76b9e9996b837beabb138e583c62d60532
Zihan Li <xiaohanor@qq.com>
2025-03-30 19:28:02 +0800

    添加UShooterCareerPage类，实现OnRetrieveMatchStats方法以处理比赛统计数据并更新成就列表

---
+++ Source/Blaster/HUD/ShooterCareerPage.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCareerPage.h"
+
+#include "Components/ScrollBox.h"
+#include "UI/HTTP/HTTPRequestTypes.h"
+#include "UI/Portal/Dashboard/CareerAchievement.h"
+
+void UShooterCareerPage::OnRetrieveMatchStats(const FDSRetrieveMatchStatsResponse& RetrieveMatchStatsResponse)
+{
+	Super::OnRetrieveMatchStats(RetrieveMatchStatsResponse);
+
+	ScrollBox_Achievements->ClearChildren();
+ 
+	TMap<FString, int32> AchievementData;
+
+	if (RetrieveMatchStatsResponse.defeats > 0) AchievementData.Emplace(TEXT("战败："), RetrieveMatchStatsResponse.defeats);
+	if (RetrieveMatchStatsResponse.scoredElims > 0) AchievementData.Emplace(TEXT("淘汰："), RetrieveMatchStatsResponse.scoredElims);
+
+	for (const TPair<FString, int32>& Pair : AchievementData)
+	{
+		UCareerAchievement* CareerAchievement = CreateWidget<UCareerAchievement>(this, CareerAchievementClass);
+		if (IsValid(CareerAchievement))
+		{
+			CareerAchievement->SetAchievementText(Pair.Key, Pair.Value);
+			ScrollBox_Achievements->AddChild(CareerAchievement);
+		}
+	}
+}
---
+++ Source/Blaster/HUD/ShooterCareerPage.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Portal/Dashboard/CareerPage.h"
+#include "ShooterCareerPage.generated.h"
+
+class UCareerAchievement;
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API UShooterCareerPage : public UCareerPage
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnRetrieveMatchStats(const FDSRetrieveMatchStatsResponse& RetrieveMatchStatsResponse) override;
+};
